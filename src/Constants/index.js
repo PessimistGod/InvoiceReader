b7c681df932c5b17c57829a3acdd5a9037c9c578
PessimistGod <rameshrahul0004@gmail.com>
2024-01-09 03:48:55 +0530

    main

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,5 +1,5 @@
 // export const backendURL = 'http://localhost:5000';
-export const backendURL = 'https://invoice-backend-nine.vercel.app';
-// export const backendURL = 'https://backend-invoice-fsdr.onrender.com';
+// export const backendURL = 'https://invoice-backend-nine.vercel.app';
+export const backendURL = 'https://backend-invoice-fsdr.onrender.com';
 
 
